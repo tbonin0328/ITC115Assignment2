1e1a2428e021aeed439b85dd4208b9f1abb7dd7a
Thomas Bonin <thomas.e.bonin@gmail.com>
2014-01-15 11:25:06 -0800

    Deletes repetitive code, color variable still a probleme

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -1,6 +1,6 @@
 import acm.graphics.*;
 import acm.program.*;
-import java.awt.*;
+import java.awt.Color;
 
 public class Assignment2 extends GraphicsProgram
 {
@@ -14,11 +14,12 @@ public class Assignment2 extends GraphicsProgram
 	private static final int BRICK_COLUMNS = 10; 
 	private static final int SEPARATION = 4; 
 	private static final int BRICK_HEIGHT = 8;
-	private static final int BRICK_WIDTH = 10; //1, 2
+	private static final int BRICK_WIDTH = APPLET_WIDTH/BRICK_COLUMNS-SEPARATION; 
 	// Calculated so that it fills the space provided 
-	//private static final int BALL_START_X = Center of Screen;
-	//private static final int BALL_START_Y = Center of Screen;
 	private static final int BALL_RADIUS = 10;
+	private static final int BALL_START_X = APPLET_WIDTH/2-BALL_RADIUS/2;
+	private static final int BALL_START_Y = APPLET_HEIGHT/2-BALL_RADIUS/2;
+	
 	
 	public void run()
 	{
@@ -27,79 +28,52 @@ public class Assignment2 extends GraphicsProgram
 		//initiate structure
 			
 		//make the first half of the staircase blue using numStairs/2 as the limit
+		
 		for (int j=0; j < BRICK_ROWS; j++)
+		{
 			if (j<2)
 			{
-			GRect rect = null;
-			makeRect(j);
-			add(rect);
+				for (int i=0; i < BRICK_COLUMNS; i++)
+					{
+						makeRect(i,j,(SEPARATION + BRICK_WIDTH),BRICK_Y_OFFSET,(BRICK_HEIGHT + SEPARATION),BRICK_WIDTH, BRICK_HEIGHT);
+					}
 			}
 			else if (j<4)
 			{
-			for (int i=0; i < BRICK_COLUMNS; i++)
-				{
-					//x value is 
-					GRect rect;
-					rect = new GRect((SEPARATION + BRICK_WIDTH)*i,BRICK_Y_OFFSET + (BRICK_HEIGHT + SEPARATION)*j,BRICK_WIDTH, BRICK_HEIGHT);
-					rect.setColor(Color.BLACK);
-					rect.setFillColor(Color.ORANGE);
-					rect.setFilled(true);
-					add(rect);
-				}//end i for loop
+				for (int i=0; i < BRICK_COLUMNS; i++)
+					{
+						makeRect(i,j,(SEPARATION + BRICK_WIDTH),BRICK_Y_OFFSET,(BRICK_HEIGHT + SEPARATION),BRICK_WIDTH, BRICK_HEIGHT);
+					}
 			}
-			else if (j<6)
-			{
-			for (int i=0; i < BRICK_COLUMNS; i++)
-				{
-					//x value is 
-					GRect rect;
-					rect = new GRect((SEPARATION + BRICK_WIDTH)*i,BRICK_Y_OFFSET + (BRICK_HEIGHT + SEPARATION)*j,BRICK_WIDTH, BRICK_HEIGHT);
-					rect.setColor(Color.BLACK);
-					rect.setFillColor(Color.YELLOW);
-					rect.setFilled(true);
-					add(rect);
-				}//end i for loop
-			}
-			else if (j<8)
-			{
-			for (int i=0; i < BRICK_COLUMNS; i++)
-				{
-					//x value is 
-					GRect rect;
-					rect = new GRect((SEPARATION + BRICK_WIDTH)*i,BRICK_Y_OFFSET + (BRICK_HEIGHT + SEPARATION)*j,BRICK_WIDTH, BRICK_HEIGHT);
-					rect.setColor(Color.BLACK);
-					rect.setFillColor(Color.GREEN);
-					rect.setFilled(true);
-					add(rect);
-				}//end i for loop
-			}
-			else if (j<10)
-			{
-			for (int i=0; i < BRICK_COLUMNS; i++)
-				{
-					//x value is 
-					GRect rect;
-					rect = new GRect((SEPARATION + BRICK_WIDTH)*i,BRICK_Y_OFFSET + (BRICK_HEIGHT + SEPARATION)*j,BRICK_WIDTH, BRICK_HEIGHT);
-					rect.setColor(Color.BLACK);
-					rect.setFillColor(Color.CYAN);
-					rect.setFilled(true);
-					add(rect);
-				}//end i for loop
-			}
-			}//end run() method
+		}//end j for loop
+		
+		makeOval();
+		makeRect(1,1,APPLET_WIDTH/2-PADDLE_WIDTH/2,0,(APPLET_HEIGHT-(PADDLE_Y_OFFSET+PADDLE_HEIGHT)),PADDLE_WIDTH,PADDLE_HEIGHT);
+	}//end run() method
 
-	public static void makeRect (int x)
+	public void makeRect (int x, int y, int a, int b, int c, int d, int e)
 	{
-	for (int i=0; i < BRICK_COLUMNS; i++)
-	{
-		//x value is 
+	//x value is 
 		GRect rect;
-		rect = new GRect((SEPARATION + BRICK_WIDTH)*i,BRICK_Y_OFFSET + (BRICK_HEIGHT + SEPARATION)*x,BRICK_WIDTH, BRICK_HEIGHT);
-		rect.setColor(Color.BLACK);
+		rect = new GRect(a*x,b+c*y,d,e);
+		//rect.setColor(Color.BLACK);
 		rect.setFillColor(Color.RED);
 		rect.setFilled(true);
+		add(rect);
+	}//end makeRect
+	
+	public void makeOval ()
+	{
+	//x value is 
+		GOval oval;
+		oval = new GOval(BALL_START_X,BALL_START_Y,BALL_RADIUS, BALL_RADIUS);
+		//rect.setColor(Color.BLACK);
+		oval.setFillColor(Color.BLACK);
+		oval.setFilled(true);
+		add(oval);
 	}//end i for loop
 }
-}
+
+
 	
 
