6fd4eed23db4a2d94a65e71443c17ed9285853fd
tbonin0328 <tbonin0328@yahoo.com>
2014-01-17 17:43:47 -0800

    last stretch

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -22,42 +22,37 @@ public class Assignment2 extends GraphicsProgram
 	private static final int BALL_START_X = APPLET_WIDTH/2-BALL_RADIUS;
 	private static final int BALL_START_Y = APPLET_HEIGHT/2-BALL_RADIUS;
 	
+	Color[]colors = {
+			Color.RED,
+			Color.ORANGE,
+			Color.YELLOW,
+			Color.GREEN,
+			Color.CYAN,
+			};
 	
 	public void run()
 	{
 		setSize(APPLET_WIDTH,APPLET_HEIGHT);
-			
-		//initiate structure
-			
-		//make the first half of the staircase blue using numStairs/2 as the limit
 		
 		for (int j=0; j < BRICK_ROWS; j++)
 		{
 			int xCoord = 0;
 			int yCoord = 0;
+			Color color = colors[(int)(j*0.5)];
+			int xFactor = SEPARATION + BRICK_WIDTH;
+			int yFactor =  BRICK_Y_OFFSET+(BRICK_HEIGHT + SEPARATION)*j;
 			
-			if (j<2)
 			{
-				
-				for (int i=0; i < BRICK_COLUMNS; i++)
-					{
-						xCoord = i*(SEPARATION + BRICK_WIDTH);
-						yCoord = BRICK_Y_OFFSET+(BRICK_HEIGHT + SEPARATION)*j;
-						makeBrick(xCoord,yCoord,BRICK_WIDTH,BRICK_HEIGHT,Color.RED);
-					}
-			}
-			else if (j<4)
-			{
-				for (int i=0; i < BRICK_COLUMNS; i++)
-					{
-						xCoord = i*(SEPARATION + BRICK_WIDTH);
-						yCoord = BRICK_Y_OFFSET+(BRICK_HEIGHT + SEPARATION)*j;	
-						makeBrick(xCoord,yCoord,BRICK_WIDTH,BRICK_HEIGHT,Color.BLACK);
-					}
-			}
-		}//end j for loop
+			for (int i=0; i < BRICK_COLUMNS; i++)
+					{					
+						xCoord = i*xFactor;
+						yCoord = yFactor;
+						makeBrick(xCoord,yCoord,BRICK_WIDTH,BRICK_HEIGHT,color);
+					} // end i loop
+			} 
+		} //end j loop
 		
-		makePaddle();
+		makeBall();
 		
 		makeBrick(APPLET_WIDTH/2-PADDLE_WIDTH/2,(APPLET_HEIGHT-(PADDLE_Y_OFFSET+PADDLE_HEIGHT)),PADDLE_WIDTH,PADDLE_HEIGHT,Color.BLACK);
 	}//end run() method
@@ -73,12 +68,11 @@ public class Assignment2 extends GraphicsProgram
 		add(rect);
 	}//end makeRect
 	
-	public void makePaddle ()
+	public void makeBall ()
 	{
 	//x value is 
 		GOval oval;
 		oval = new GOval(BALL_START_X,BALL_START_Y,BALL_RADIUS*2, BALL_RADIUS*2);
-		//rect.setColor(Color.BLACK);
 		oval.setFillColor(Color.BLACK);
 		oval.setFilled(true);
 		add(oval);
